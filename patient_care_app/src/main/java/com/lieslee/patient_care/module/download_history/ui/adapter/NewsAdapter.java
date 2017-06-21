8bd9a322f3c51c93c0e200a72e5741f920a0e697
LiesLee <LiesLee@foxmail.com>
2017-06-21 23:09:56 +0800

    news detail ui

--- patient_care_app/src/main/java/com/lieslee/patient_care/module/download_history/ui/adapter/NewsAdapter.java
+++ patient_care_app/src/main/java/com/lieslee/patient_care/module/download_history/ui/adapter/NewsAdapter.java
@@ -1,9 +1,14 @@
 package com.lieslee.patient_care.module.download_history.ui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.common.base.ui.BaseAdapter;
+import com.lieslee.patient_care.R;
+import com.lieslee.patient_care.module.news.ui.activity.NewsDetailActivity;
 
 import java.util.List;
 
@@ -19,6 +24,29 @@ public class NewsAdapter extends BaseAdapter<String> {
 
     @Override
     protected void convert(BaseViewHolder baseViewHolder, String s) {
+        RoundCornerProgressBar rvpb_bar = baseViewHolder.getView(R.id.rvpb_bar);
+        int position = getFinalPositionOnList(baseViewHolder);
+        if(position == 0){
+            baseViewHolder.setBackgroundColor(R.id.tv_download_tips, mContext.getResources().getColor(R.color.pink_green));
+            baseViewHolder.setText(R.id.tv_download_tips, "已下载");
+            baseViewHolder.getView(R.id.ll_progress_layout).setVisibility(View.INVISIBLE);
+        }else if(position == 1){
+            baseViewHolder.setBackgroundColor(R.id.tv_download_tips, mContext.getResources().getColor(R.color.pink_blue));
+            baseViewHolder.setText(R.id.tv_download_tips, "下载中");
+            baseViewHolder.getView(R.id.ll_progress_layout).setVisibility(View.VISIBLE);
+            rvpb_bar.setProgress(50f);
+        }else{
+            baseViewHolder.setBackgroundColor(R.id.tv_download_tips, mContext.getResources().getColor(R.color.line_color));
+            baseViewHolder.setText(R.id.tv_download_tips, "未下载");
+            baseViewHolder.getView(R.id.ll_progress_layout).setVisibility(View.VISIBLE);
+            rvpb_bar.setProgress(2f);
+        }
 
+        baseViewHolder.getConvertView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.startActivity(new Intent(mContext, NewsDetailActivity.class));
+            }
+        });
     }
 }
---
+++ patient_care_app/src/main/java/com/lieslee/patient_care/module/news/ui/activity/NewsDetailActivity.java
@@ -0,0 +1,29 @@
+package com.lieslee.patient_care.module.news.ui.activity;
+
+import android.view.View;
+
+import com.common.annotation.ActivityFragmentInject;
+import com.common.base.ui.BaseActivity;
+import com.lieslee.patient_care.R;
+
+/**
+ * Created by LiesLee on 2017/6/21.
+ * Email: LiesLee@foxmail.com
+ */
+@ActivityFragmentInject(contentViewId = R.layout.act_news_detail, toolbarTitle = R.string.news_detail)
+public class NewsDetailActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
