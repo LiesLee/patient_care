34caaffb850e658ba92897d32a873adb4934e285
liyongjian@vip-time.cn <huagege12358>
2017-09-18 22:57:48 +0800

    release 1

--- patient_care_app/src/main/java/com/lieslee/patient_care/http/protocol/BaseProtocol.java
+++ patient_care_app/src/main/java/com/lieslee/patient_care/http/protocol/BaseProtocol.java
@@ -43,29 +43,7 @@ public class BaseProtocol {
         }
         return sb.toString();
     }
-    public static String getParams1(SortedMap<String, String> parameters) {
-        StringBuffer sb = new StringBuffer();
-        Set es = parameters.entrySet();//所有参与传参的参数按照accsii排序（升序）
-        Iterator it = es.iterator();
-        while (it.hasNext()) {
-            Map.Entry entry = (Map.Entry) it.next();
-            String k = (String) entry.getKey();
-            Object v = entry.getValue();
-            //if (null != v && !"".equals(v)) {
-            if (null != v) {
-                sb.append(k + "=" + v + "&");
-            }
-        }
-        return sb.toString();
-    }
 
-    public static String createSign(String characterEncoding, SortedMap<String, Object> parameters) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(getParams(parameters));
-        sb.append(HttpConstants.getSignKey());
-        String sign = MD5Util.MD5Encode(sb.toString(), characterEncoding);
-        return sign;
-    }
 
     public static String createSign(SortedMap<String, Object> parameters) {
         StringBuffer sb = new StringBuffer();
@@ -74,13 +52,7 @@ public class BaseProtocol {
         String sign = MD5Util.MD5Encode(sb.toString(), "utf-8");
         return sign;
     }
-    public static String createSign1(SortedMap<String, String> parameters) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(getParams1(parameters));
-        sb.append(HttpConstants.getSignKey());
-        String sign = MD5Util.MD5Encode(sb.toString(), "utf-8");
-        return sign;
-    }
+
 
     public static SortedMap<String, Object> createPatams(Map<String, Object> parameters, String urlName){
         HashMap<String, Object> hashMap = new HashMap<>(parameters);
@@ -103,30 +75,6 @@ public class BaseProtocol {
 
         SortedMap<String, Object> map = new TreeMap<>(hashMap);
         map.put("api_sign", createSign(map));
-        map.put("a", urlName);
-        return map;
-    }
-
-    public static SortedMap<String, String> createPatams1(Map<String, String> parameters, String urlName){
-        HashMap<String, String> hashMap = new HashMap<>(parameters);
-        try {
-            hashMap.put("app_version", ""+PatientCareApplication.getInstance().getPackageManager().getPackageInfo(PatientCareApplication.getInstance().getPackageName(),PackageManager.GET_META_DATA).versionName);
-            hashMap.put("client_time",""+System.currentTimeMillis());
-            hashMap.put("system_version",Build.VERSION.RELEASE);
-            hashMap.put("device_id", Util.getDeviceId());
-            hashMap.put("utm_medium", "android");
-            hashMap.put("utm_source", "vmApp");
-
-            if(!hashMap.containsKey("api_ver")){
-                hashMap.put("api_ver", PatientCareApplication.getInstance().getResources().getString(R.string.api_ver));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        SortedMap<String, String> map = new TreeMap<>(hashMap);
-        map.put("api_sign", createSign1(map));
         map.put("a", urlName);
         return map;
     }
